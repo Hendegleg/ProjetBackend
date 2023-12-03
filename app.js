95047d32ad4f00d560fa8d779d1f80adf06bbaa4
hendlegleg <hendlegleg1@gmail.com>
2023-12-03 18:34:50 +0100

    firstt commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+
+mongoose
+.connect(
+     "mongodb://127.0.0.1:27017/DS",
+   { useNewUrlParser: true, useUnifiedTopology: true}
+)
+.then(()=>console.log("connexion a mongoDB reussite"))
+.catch((e) =>console.log("connexion a mongoDB echouée", e))
+
+const app = express();
+
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  next();
+});
+
+app.use(express.json());
+
+
+
+
+module.exports = app;
---
+++ models/Candidat.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function (v) {
+                return /\S+@\S+\.\S+/.test(v);
+            },
+            message: 'L\'adresse e-mail doit contenir le caractère "@".',
+        },
+    },
+    password: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    role: {
+        type: String,
+        enum: ["admin", "user"],
+    }
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
